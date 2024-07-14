05bd8f5bb5a36c6d1e4821f34e2ef69ccde5697a
Andrew Gardner <vardantexcode@gmail.com>
2024-07-14 17:11:17 -0400

    July 2024 Update

---
+++ test.js
@@ -0,0 +1,13 @@
+// Get average of scores
+
+function getAverage(scores) {
+    for( let i = 0; i < scores.lenght; i++) {
+      let total = 0;
+      total += scores;
+      return total / i;
+    
+    }
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+
